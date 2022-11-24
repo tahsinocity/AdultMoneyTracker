34e7b31e1d4d3c9fe9e665c36474e53fc0753ae4
Tahsin Ahmed <tahsinahmed95@gmail.com>
2022-11-24 18:16:55 -0500

    Updated itemized page

--- screens/Itemized.js
+++ screens/Itemized.js
@@ -1,12 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { db, auth } from '../config';
+import { doc, onSnapshot, collection } from 'firebase/firestore';
+import Colors from "../constants/Colors";
 
-const Itemized = () => {
+export default function Itemized({ navigation }) {
+  const [transactions, setTransactions] = useState([]);
+  const currentUserUID = auth.currentUser.uid;
+  const docRef = doc(db, "users", currentUserUID);
+  const colRef = collection(docRef, 'transactions');
+
+  useEffect(() => {
+    const unsub = onSnapshot(colRef, (doc) => {
+      doc.forEach(doc => {
+        setTransactions(prevArr => [...prevArr, {data: doc.data(), id: doc.id}])
+      })
+    })
+
+    return () => unsub();
+  }, [])
+
   return (
-    <View>
-      <Text>Itemized</Text>
+    <View style={styles.container}>
+      <View style={{flex: 1}}>
+        <Text style={{alignSelf: "center", color: Colors.gray, fontSize: 36, marginBottom: 24, marginTop: 50, borderBottom: `1px solid ${Colors.gray}`}}>
+          Recent Transactions
+        </Text>
+        {transactions?.length && transactions.map(transaction => {
+          const {amount, category, description, split, timestamp, totalSpent} = transaction.data
+          const formattedTime = new Date(timestamp.seconds*1000)
+          const date = formattedTime.getDate()
+          const month = formattedTime.toLocaleString('default', { month: 'long' })
+
+          return(
+            <View key={transaction.id} style={{border: `1px solid ${Colors.gray}`, borderRadius: 10, margin: 10, padding: 10}}>
+              <Text style={{ fontSize: 16, fontWeight: "bold", color: Colors.gray, alignSelf: "start"}}>{date} {month}</Text>
+              <View style={{margin: 5, flexDirection: "row"}}>
+                <View style={{flex: 1, borderRight: `1px solid ${Colors.gray}`, margin: 10}}>
+                  <Text style={styles.label}>{description}</Text>
+                  <Text style={styles.label}>{category}</Text>
+                </View>
+                <View style={{flex: 1, margin: 10}}>
+                  <Text style={styles.label}>Amount: $ {amount}</Text>
+                  <Text style={styles.label}>Split: $ {split}</Text>
+                </View>
+              </View>
+              <Text style={{ fontSize: 16, fontWeight: "bold", color: Colors.gray, alignSelf: "end"}}>Total Spent: $ {totalSpent}</Text>
+            </View>
+          )
+        })}
+      </View>
     </View>
-  )
+  );
 }
 
-export default Itemized
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.navyBlue,
+    overflowY: 'scroll',
+    justifyContent: "space-between",
+    alignItems: "stretch"
+  },
+  label : { fontSize: 16, fontWeight: "bold", color: Colors.gray },
+  header : { fontSize: 42, color: Colors.gray, alignSelf: "center" }
+})
